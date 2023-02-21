c0bbf81bf8ce248bdf42acc77715520af9afe40a
Derrick <djmulinde@gmail.com>
2023-02-21 20:08:38 +0300

    Add some greetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Message.create(greetings: "Howdie partner?")
+Message.create(greetings: "How are you Oga?")
+Message.create(greetings: "Warap Charle?")
+Message.create(greetings: "Bundia senior cheffe")
+Message.create(greetings: "Coma estas Amigo")
